b397a96c8321a52a0ce737af392a77000227b019
Somel <somelahmed88@gmail.com>
2023-10-25 23:20:10 +0600

    get products by BrandName Added

--- index.js
+++ index.js
@@ -44,10 +44,48 @@ async function run() {
         res.send(result);
     })
 
+
+
+app.get('/productsByBrand/:brandname', async (req, res) => {
+    const brandname = req.params.brandname;
+  
+    
+      const result = await productCollection.find({ brand: brandname }).toArray();
+      res.json(result);
+    } 
+  );
+  
+  
+  
+      
+      
+
     app.post('/products', async(req, res) => {
         const newProduct = req.body;
         console.log(newProduct);
         const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    })
+
+    app.put('/products/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options = {upsert: true};
+        const updatedProduct = req.body;
+        const product = {
+            $set: {
+                image : updatedProduct.image,
+                name : updatedProduct.name,
+                brand : updatedProduct.brand,
+                type: updatedProduct.type,
+                price : updatedProduct.price,
+                shortDescription: updatedProduct.shortDescription,
+                rating: updatedProduct.rating
+                
+
+            },
+        };
+        const result = await productCollection.updateOne(filter, product, options);
         res.send(result);
     })
 
